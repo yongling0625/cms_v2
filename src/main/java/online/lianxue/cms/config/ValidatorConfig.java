30958a3358e94cdac8dd8881851995a2204ab5a0
zhuyongling <79001833@qq.com>
2019-09-29 14:35:35 +0800

    optimize code

--- src/main/java/online/lianxue/cms/config/ValidatorConfig.java
+++ src/main/java/online/lianxue/cms/config/ValidatorConfig.java
@@ -6,19 +6,17 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 
 @Configuration
 public class ValidatorConfig {
 
     @Bean
     public Validator validator() {
-        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class)
+        return Validation
+                .byProvider(HibernateValidator.class)
                 .configure()
-                .failFast(true)// true-快速失败返回模式    false-普通模式
-                .buildValidatorFactory();
-
-        Validator validator = validatorFactory.getValidator();
-        return validator;
+                .failFast(true)
+                .buildValidatorFactory()
+                .getValidator();
     }
 }
